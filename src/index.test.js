f33d34114fe5969f2fa1619ce260eae170e42dbb
subeshb1 <subeshb1@gmail.com>
2018-10-13 02:05:29 +0545

    chore(build): Add travis Config, conventional commit  and semantic release

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+const NepaliDate = require("./index");
+
+describe("NepaliDate to English", () => {
+  it(" testing toAD ", () => {
+    const nd = new NepaliDate(2054, 5, 24);
+    expect(nd.toAD()).toEqual({ year: 1997, month: 9, date: 10 });
+
+    const empty = new NepaliDate();
+    expect(empty.toAD()).toEqual({ year: 1943, month: 3, date: 14 });
+  });
+});
