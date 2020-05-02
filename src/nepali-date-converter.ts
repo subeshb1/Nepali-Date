82708bf2953a8f5daa48dd193796f4ef22a58738
Subesh Bhandari <subeshb1@gmail.com>
2020-05-03 00:27:37 +0545

    Add skeletion method for library

--- src/nepali-date-converter.ts
+++ src/nepali-date-converter.ts
@@ -1,6 +1,57 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
 
+import monthDaysMappings from './month-days-mapping'
+
+const KTM_OFFSET = 20700000;
+
+export default class NepaliDate {
+  private date:number;
+  private month:number;
+  private year:number;
+  constructor();
+  constructor(dateString: string);
+  constructor(year: number, month: number, date: number);
+  constructor(data?: any) {
+    this.date = 1;
+    this.month = 1;
+    this.year = 1;
+  }
+
+  toJsDate(): Date {
+
+    return new Date()
+  }
+
+
+  getDate(): number {
+    return this.date
+  }
+
+  setDate() {
+
+  }
+
+  getMonth(): number {
+    return this.month
+  }
+
+  setMonth() {
+    
+  }
+
+  getYear(): number {
+    return this.year
+  }
+
+  setYear() {
+
+  }
+
+
+  static now() {
+    return Date.now();
+  }
+
+  static fromAD() {
+
+  }
 }
