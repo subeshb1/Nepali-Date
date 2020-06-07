67478b14949106f78c4cd351add0bd4f9ae06a36
Subesh Bhandari <subeshb1@gmail.com>
2020-06-07 16:59:36 +0545

    Add basic tests for NepaliDate

--- test/nepali-date-converter.test.ts
+++ test/nepali-date-converter.test.ts
@@ -1,14 +1,13 @@
 import NepaliDate from "../src/nepali-date-converter"
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+describe("NepaliDate to English", () => {
+  it("testing toAD ", () => {
+    const nd = new NepaliDate(2054, 5, 24);
+    expect(nd.getAD()).toEqual({ year: 1997, month: 9, date: 10, day: 5 });
+  });
 
-  it("NepaliDate is instantiable", () => {
-    expect(new NepaliDate()).toBeInstanceOf(NepaliDate)
-  })
-})
+  it("test fromAd", () => {
+    const nd = NepaliDate.fromAD(new Date(1997, 9, 10));
+    expect(nd.getBS()).toEqual(new NepaliDate(2054, 5, 24).getBS());
+  });
+});
